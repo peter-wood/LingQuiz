e7a750b174b9b263f02f3292d5e303241604cfc8
Peter Wood <peter.wood@usask.ca>
2014-09-04 01:28:27 -0600

    fixed relative pathnames in server

--- app/lib/config.js
+++ app/lib/config.js
@@ -1,10 +1,1 @@
-var config = {}
-config.setup = 'pw on sammy';
-config.deployment = 'production';
-config.db = 'Ling111PW';
-config.port = 8000;
-config.slides = '/home/peter/nodejs/LingQuiz/app/resources/slides/';
-config.handouts = '/home/peter/nodejs/LingQuiz/app/resources/handouts/';
-config.book = '/home/peter/nodejs/LingQuiz/app/resources/book/';
-
-module.exports = config;
+pw_george.js
