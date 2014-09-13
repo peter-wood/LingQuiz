2329f913df4c01e1c56a5e712b683b781527e2c6
Peter Wood <peter.wood@usask.ca>
2014-09-12 23:05:35 -0600

    added bash script for apache deployment

--- app/lib/testing_doctor.js
+++ app/lib/testing_doctor.js
@@ -1,10 +1,15 @@
 var config = {}
-config.setup = 'george';
+config.setup = 'doctor';
 config.deployment = 'testing';
 config.port = 8080;
-config.slides = '/home/peter/Development/nodejs/LingTesting/app/resources/slides/';
-config.handouts = 'home/peter/Development/nodejs/LingTesting/app/resources/handouts/';
-config.book = 'home/peter/Development/nodejs/LingTesting/app/resources/book/';
+config.slides = '/home/peter/Development/nodejs/LingQuiz/app/resources/slides/';
+config.handouts = 'home/peter/Development/nodejs/LingQuiz/app/resources/handouts/';
+config.book = 'home/peter/Development/nodejs/LingQuiz/app/resources/book/';
 config.db = 'LingTesting';
+// @@webroot@@=/var/www/localhost/htdocs
+// @@webdir@@=Ling111_doctor
+// @@nodeserver@@=http://localhost
+// @@nodeport@@=8080
+// @@user@@=apache
 
 module.exports = config;
