7b44b97359f8d5b70d6613050d1477df6bd461a8
Peter Wood <peter.wood@usask.ca>
2014-09-15 09:26:09 -0600

    working on book

--- app/lib/testing_doctor.js
+++ app/lib/testing_doctor.js
@@ -1,11 +1,11 @@
 var config = {}
-config.setup = 'doctor';
-config.deployment = 'testing';
+config.setup = 'beta testing on doctor';
+config.deployment = 'production';
 config.port = 8080;
 config.slides = '/home/peter/Development/nodejs/LingQuiz/app/resources/slides/';
-config.handouts = 'home/peter/Development/nodejs/LingQuiz/app/resources/handouts/';
-config.book = 'home/peter/Development/nodejs/LingQuiz/app/resources/book/';
-config.db = 'LingTesting';
+config.handouts = '/home/peter/Development/nodejs/LingQuiz/app/resources/handouts/';
+config.book = '/home/peter/Development/nodejs/LingQuiz/app/resources/book/';
+config.db = 'LingTestDoctor';
 // @@webroot@@=/var/www/localhost/htdocs
 // @@webdir@@=Ling111_doctor
 // @@nodeserver@@=http://localhost
