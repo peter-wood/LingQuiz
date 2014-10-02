4e22a9b6632119e63f5776304c38c4c37a460a63
Peter Wood <peter.wood@usask.ca>
2014-10-02 02:07:10 -0600

    quiz server wood online

--- app/lib/pw_george.js
+++ app/lib/pw_george.js
@@ -1,6 +1,6 @@
 var config = {}
 config.setup = 'george';
-config.deployment = 'testing'; // testing uses a test database, production uses a life database
+config.deployment = 'production'; // testing uses a test database, production uses a life database
 config.port = 8000;
 config.slides = '/storage/nodejs/LingQuiz/app/resources/slides/';
 config.handouts = '/storage/nodejs/LingQuiz/app/resources/handouts/';
@@ -8,12 +8,12 @@ config.book = '/storage/nodejs/LingQuiz/app/resources/book/';
 config.db = 'Ling111PW';
 config.loginValidFor = 45; // in minutes
 config.purgeInterval = 30; // in seconds
-config.quizzes = [
-    { 'quiz': 'Practice-1', 'open': true, time: 15, numQuestions: 10, save:false, retake: true },
-    { 'quiz': 'Quiz-1', 'open': false, time: 50, numQuestions: 30, save: true, retake: false }
-];
+config.quizzes = {
+    'Practice-1': { 'type': 'practice', 'open': true, 'time': 15, 'numQuestions': 10, 'retake': true, 'review': true },
+    'Quiz-1': { 'type': 'quiz', 'open': false, 'time': 50, 'numQuestions': 30, 'retake': false, 'review': false }
+};
 // @@webroot@@=/storage
-// @@webdir@@=ling111_testing
+// @@webdir@@=Ling111
 // @@nodeserver@@=http://linguistics.usask.ca
 // @@nodeport@@=8000
 // @@user@@=peter
