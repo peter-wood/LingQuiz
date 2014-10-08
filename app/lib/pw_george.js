243fe93d5ed7248d780983b1c09173cc966effae
Peter Wood <peter.wood@usask.ca>
2014-10-08 17:05:14 -0600

    added practice 2

--- app/lib/pw_george.js
+++ app/lib/pw_george.js
@@ -10,6 +10,7 @@ config.loginValidFor = 45; // in minutes
 config.purgeInterval = 30; // in seconds
 config.quizzes = {
     'Practice-1': { 'type': 'practice', 'open': true, 'time': 15, 'numQuestions': 10, 'retake': true, 'review': true },
+    'Practice-2': { 'type': 'practice', 'open': true, 'time': 15, 'numQuestions': 10, 'retake': true, 'review': true },
     'Quiz-1': { 'type': 'quiz', 'open': false, 'time': 50, 'numQuestions': 30, 'retake': false, 'review': true }
 };
 // @@webroot@@=/storage
