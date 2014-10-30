058d8be7e15920025436e34f1ba563449a1f33f6
Peter Wood <peter.wood@usask.ca>
2014-10-30 00:05:32 -0600

    reenabled logging

--- app/lib/testing_george.js
+++ app/lib/testing_george.js
@@ -1,6 +1,6 @@
 var config = {}
 config.setup = 'george';
-config.deployment = 'testing'; // testing uses a test database, production uses a life database
+config.deployment = 'production'; // testing uses a test database, production uses a life database
 config.port = 8080;
 config.slides = '/storage/nodejs/LingTesting/app/resources/slides/';
 config.handouts = '/storage/nodejs/LingTesting/app/resources/handouts/';
@@ -14,7 +14,8 @@ config.quizzes = {
     'Practice-3': { 'type': 'practice', 'open': true, 'time': 15, 'numQuestions': 10, 'retake': true, 'review': true },
     'Quiz-1': { 'type': 'quiz', 'open': true, 'time': 50, 'numQuestions': 30, 'retake': false, 'review': true },
     'Quiz-2': { 'type': 'quiz', 'open': true, 'time': 50, 'numQuestions': 30, 'retake': false, 'review': true },
-    'Quiz-3': { 'type': 'quiz', 'open': true, 'time': 50, 'numQuestions': 30, 'retake': false, 'review': true }
+    'Quiz-3': { 'type': 'quiz', 'open': true, 'time': 50, 'numQuestions': 30, 'retake': false, 'review': true },
+    'Quiz-3-Retake': { 'type': 'quiz', 'open': true, 'time': 50, 'numQuestions': 30, 'retake': true, 'review': true }
 };
 // @@webroot@@=/storage
 // @@webdir@@=ling111_testing
